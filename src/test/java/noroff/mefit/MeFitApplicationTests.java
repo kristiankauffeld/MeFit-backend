9942872a0f49e62968ba4f3b49e1057b62389aaf
Kristian Andreasen <kristian.andreasen@fi.experis.com>
2023-03-06 10:09:56 +0100

    init backend

---
+++ src/test/java/noroff/mefit/MeFitApplicationTests.java
@@ -0,0 +1,13 @@
+package noroff.mefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
