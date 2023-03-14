c23e8195cf215c5f7f9eadf63ddeedad6083bd01
Lass3819 <102652990+Lass3819@users.noreply.github.com>
2023-03-14 10:43:16 +0100

    adding all service and serviceImpl files

---
+++ src/main/java/noroff/mefit/repositories/SetCountRepository.java
@@ -0,0 +1,7 @@
+package noroff.mefit.repositories;
+
+import noroff.mefit.models.SetCount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SetCountRepository extends JpaRepository<SetCount, Integer> {
+}
