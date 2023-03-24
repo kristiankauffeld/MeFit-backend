30df1a2b9681c478b2017e4a4b4c2167768ddb96
Kristian K. Andreasen <kristian.kauffeld@gmail.com>
2023-03-24 13:25:10 +0100

    changed the gradle config file in order for deployment on railway to work

--- src/main/java/noroff/mefit/config/SecurityConfig.java
+++ src/main/java/noroff/mefit/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
                 .csrf().disable()
                 // Enable security for http requests
                 .authorizeHttpRequests(authorize -> authorize
-                      .requestMatchers("/api/v1/programs",    "/api/v1/programs/**").permitAll()//TODO clean up
+                    /*  .requestMatchers("/api/v1/programs",    "/api/v1/programs/**").permitAll()//TODO clean up
                         .requestMatchers("/api/v1/goals",       "/api/v1/goals/**").permitAll()//TODO clean up
                         .requestMatchers("/api/v1/workouts",    "/api/v1/workouts/**").permitAll()//TODO clean up
                         .requestMatchers("/api/v1/set_counts",  "/api/v1/set_counts/**").permitAll()//TODO clean up
@@ -27,7 +27,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/user_accs",   "/api/v1/user_accs/**").permitAll()//TODO clean up
                         .requestMatchers("/api/v1/addresses",   "/api/v1/addresses/**").permitAll()//TODO clean up
                         .requestMatchers("/api/v1/exercises",   "/api/v1/exercises/**").permitAll()//TODO clean up
-                        .requestMatchers("/api/v1/activities",    "/api/v1/activities/**").permitAll()//TODO clean up
+                        .requestMatchers("/api/v1/activities",    "/api/v1/activities/**").permitAll()//TODO clean up */
 
 
                         //.requestMatchers("/api/v1/resources/authorized").hasAnyAuthority()
